6f30ed7c0c4dc0ab77ea1af67b991a61a6eed58f
Roger <roger@mentra.glass>
2025-06-18 12:32:14 -0700

    add webhook endpoint

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,10 @@ export default defineConfig({
       '/webview': {
         target: 'http://localhost:3000',
         changeOrigin: true
+      },
+      '/webhook': {
+        target: 'http://localhost:3000',
+        changeOrigin: true
       }
     }
   },
